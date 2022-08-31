e2412315fe269de2d24fad66fd5af395a568c073
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-08-31 11:56:30 +0530

    Loading..

---
+++ projeci1_oopsRev/src/projeci1_oopsRev/Ex1.java
@@ -0,0 +1,16 @@
+package projeci1_oopsRev;
+
+public class Ex1 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic 
+        System.out.println("Hi");
+        System.out.print("Java");
+        System.out.print("Project");
+        System.out.println("How are " + "you ?" + 99);
+        System.out.println(7+5);
+        System.out.println("7"+"5"); 
+    }
+    
+}
